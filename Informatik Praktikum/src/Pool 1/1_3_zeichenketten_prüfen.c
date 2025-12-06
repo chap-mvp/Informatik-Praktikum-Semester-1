fa698e5a8e810a6a36731018e0f0b1bf2d66f750
Issmaeel <issmaeel.akram@gmail.com>
2025-12-07 00:10:25 +0100

    Add initial project files and Pool 1 exercises
    Added VS Code configuration files (launch.json, tasks.json), a Makefile for building and running the project, and source files for Pool 1 exercises: 1_1_kleines_gluecksspiel.c (number guessing game), 1_2_zeichenkette_ordnen.c (string sorting), and stubs for 1_3_zeichenketten_prüfen.c and 1_4_statistische_auswertung_einer_datenreihe.c. This sets up the project structure and provides implementations for the first two exercises.

---
+++ Informatik Praktikum/src/Pool 1/1_3_zeichenketten_prüfen.c
@@ -0,0 +1,8 @@
+/*
+ Filename  : 1_3_zeichenketten_prüfen.c
+ Program   : 
+ Input     : 
+ Output    : 
+ Author    : Akram, M. Issmaeel
+ Version   : V01 - 06.12.2025
+ */
---
+++ Informatik Praktikum/src/Pool 1/1_4_statistische_auswertung_einer_datenreihe.c
@@ -0,0 +1,8 @@
+/*
+ Filename  : 1_4_statistische_auswertung_einer_datenreihe.c
+ Program   : 
+ Input     : 
+ Output    : 
+ Author    : Akram, M. Issmaeel
+ Version   : V01 - 06.12.2025
+ */
