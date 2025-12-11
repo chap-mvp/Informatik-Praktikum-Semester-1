81a5095e8037018245bb0eb9d604139203338f37
Issmaeel <issmaeel.akram@gmail.com>
2025-12-11 08:26:58 +0100

    Mark checklist item as complete in string check program
    Updated the checklist in 1_3_zeichenketten_pruefen.c to mark the output requirement as completed. Minor comment and version string changes were also made.

--- Informatik Praktikum/src/1_3_zeichenketten_pruefen.c
+++ Informatik Praktikum/src/1_3_zeichenketten_pruefen.c
@@ -4,13 +4,13 @@
  Input     :
  Output    :
  Author    : Akram, M. Issmaeel
- Version   : V01 - 07.12.2025
+ Version   : V01 - 07.12.2025d
 
  Checklist  :
     ✓ - die Zeichenketten z_1 und z_2 von der Tastatur einlesen und auf zulässige Zeichen prüfen (Falls unzulässige Zeichen vorkommen, ist eine Fehlermeldung auf dem Bildschirm auszugeben.)
     ✓ - die Längen der Zeichenketten ermitteln
     ✓ - prüfen ob die kürzere der beiden Zeichenketten in der anderen Zeichenkette enthalten ist und dabei die Position in der längeren Zeichenkette ermitteln, ab der zum ersten Mal eine Übereinstimmung vorliegt
-    ✗ - in einer Zeile auf dem Bildschirm folgendes ausgeben: die längere Zeichenkette, die kürzere Zeichenkette, das Testergebnis als Text (z.B. „z2 ist in zl enthalten") und die Position, ab der die kürzere Zeichenkette erstmals in der längeren enthalten ist.
+    ✓ - in einer Zeile auf dem Bildschirm folgendes ausgeben: die längere Zeichenkette, die kürzere Zeichenkette, das Testergebnis als Text (z.B. „z2 ist in zl enthalten") und die Position, ab der die kürzere Zeichenkette erstmals in der längeren enthalten ist.
  */
 
 #include <stdio.h>
@@ -107,10 +107,9 @@ int main()
             }
         }
     }
-    // printf("%d", smolStrandSize);
     printf("\nnub");
 }
 
-// ACCADCAAACDC
+// ACCADCAAACDCACCADCAAACDCACCADCAAACDC
 // ACCADCAAACDD
 // ACDC
